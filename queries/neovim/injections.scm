66c26e141f23c2878b884e2d6fba2c343bfc370a
Noor Wachid <wachid@fisip.net>
2024-03-06 11:04:04 +0700

    update inquection queries

--- queries/neovim/injections.scm
+++ queries/neovim/injections.scm
@@ -5,3 +5,20 @@
 (code_inline_lang
   (code_inline_content) @injection.content
   (label) @injection.language)
+
+; common aliases
+
+(code_block_lang
+  (code_lang) @_language
+  (#any-of? @_language "js")
+  (code_content) @javascript)
+
+(code_block_lang
+  (code_lang) @_language
+  (#any-of? @_language "py")
+  (code_content) @python)
+
+(code_block_lang
+  (code_lang) @_language
+  (#any-of? @_language "sh" "zsh")
+  (code_content) @bash)
